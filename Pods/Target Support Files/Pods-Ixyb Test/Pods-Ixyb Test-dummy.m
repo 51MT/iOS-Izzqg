2954e383530a9c8dbde546fc114016d22e6e20a5
dzgmac <dzgmac@163.com>
2018-02-02 12:31:52 +0800

    resolve problem

--- Pods/Target Support Files/Pods-Ixyb Test/Pods-Ixyb Test-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Ixyb_Test : NSObject
-@end
-@implementation PodsDummy_Pods_Ixyb_Test
-@end
--- Pods/Target Support Files/Pods-Ixyb/Pods-Ixyb-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Ixyb : NSObject
-@end
-@implementation PodsDummy_Pods_Ixyb
-@end
